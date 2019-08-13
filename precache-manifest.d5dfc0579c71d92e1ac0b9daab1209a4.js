be6c2f83d6e733e9733b9683c04b4d13e2375f9f
Monika G Nikolova <44558797+monikanikolova@users.noreply.github.com>
2019-08-13 15:44:10 -0400

    Updates

---
+++ precache-manifest.d5dfc0579c71d92e1ac0b9daab1209a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1652e43979a9780ea22d36558a23e2",
+    "url": "/monikanikolova/angular-test-app/index.html"
+  },
+  {
+    "revision": "24d9da10a60d4f62a436",
+    "url": "/monikanikolova/angular-test-app/static/css/main.8c6517f1.chunk.css"
+  },
+  {
+    "revision": "43a1b3f62da18b22d71b",
+    "url": "/monikanikolova/angular-test-app/static/js/2.10794171.chunk.js"
+  },
+  {
+    "revision": "24d9da10a60d4f62a436",
+    "url": "/monikanikolova/angular-test-app/static/js/main.c3b90f7a.chunk.js"
+  },
+  {
+    "revision": "673fa0a3eb933338898d",
+    "url": "/monikanikolova/angular-test-app/static/js/runtime~main.ba86ae63.js"
+  }
+]);
